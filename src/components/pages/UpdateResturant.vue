ca32ced4c136e52a5824962ffd3df3e8d34cf748
Niel22 <niel2264@gmail.com>
2025-02-04 11:07:54 +0100

    Edit Update Function implemented

--- src/components/pages/UpdateResturant.vue
+++ src/components/pages/UpdateResturant.vue
@@ -2,11 +2,11 @@
     <AppHeader />
     <h1>Update Resturant</h1>
     <p style="color: red;">{{ error }}</p>
-    <form class="add" @submit.prevent="create">
+    <form class="add" @submit.prevent="update">
         <input type="text" v-model="name" placeholder="Enter name">
         <input type="text" v-model="contact" placeholder="Enter contact">
         <input type="text" v-model="address" placeholder="Enter address">
-        <button type="submit">Create</button>
+        <button type="submit">Update</button>
     </form>
 </template>
 
@@ -26,6 +26,7 @@ export default {
             contact: '',
             address: '',
             error: '',
+            id: this.$route.params.id
         }
     },
     mounted(){
@@ -33,11 +34,7 @@ export default {
             this.$router.push({name: 'signin'});
         }
 
-        this.getResturant().then(found => {
-            if(!found){
-                this.$router.push({name: 'home'});
-            }
-        })
+        this.getResturant();
     },
     methods: {
         checkAuth(){
@@ -48,20 +45,41 @@ export default {
             this.user = JSON.parse(user);
             return true;
         },
-        async getResturant(){
-            try{
-                const resturant = await axios.get('http://localhost:3000/resturants?id='+this.$route.params.id);
-            if(resturant.data.length === 0){
-                return false;
-            }
+        async getResturant() {
+            try {
+                const resturant = await axios.get('http://localhost:3000/resturants/' + this.id );
 
-            this.name = resturant.data[0].name;
-            this.contact = resturant.data[0].contact;
-            this.address = resturant.data[0].address;
-            return true;
-        }catch(error){
-            console.log(error.message);
-        }
+                this.name = resturant.data.name;
+                this.contact = resturant.data.contact;
+                this.address = resturant.data.address;
+                return true;
+            } catch (error) {
+                console.log(error.message);
+                this.$router.push({name: 'home'});
+            }
+        },
+        async update(){
+            try{
+                if (!this.name || !this.contact || !this.address) {
+                    this.error = "All fields are required";
+                    return;
+                }
+
+                const resturant = await axios.put('http://localhost:3000/resturants/'+ this.id, {
+                    name: this.name,
+                    contact: this.contact,
+                    address: this.address,
+                });
+
+                if (resturant.status !== 200) {
+                    this.error = "Problem Occured";
+                }
+                this.$router.push({ name: 'home' });
+                
+
+            }catch(error){
+                this.error = error.message;
+            }
         }
     }
 }
